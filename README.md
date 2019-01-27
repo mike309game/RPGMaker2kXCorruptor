08f185fbbc696a962723b2e5e8796caa7decca6e
mike309game <mike309game@hotmail.com>
2019-01-27 17:30:04 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+-==WARNINGs==-
+ vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+> 0. MAKE SURE TO BACK UP THE GAME <
+ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+1. You need a basic understanding on how to use command line programs.
+2. The tool only reads .png and .bmp files, if there are .xyz files you need to convert them yourself.
+
+-==HOW TO USE==-
+
+1. Place the executable in the game folder where files named RPG_RT reside.
+2. Run the program with the desired arguments.
+3. After done, close the command prompt and a new folder called "Randomized" was created.
+4. Copy all the folders in Randomized to the previous folder, replace everything.
+5. Done! enjoy your mess of a game!
+
+-==ARGUMENTS==-
+
+-name : Shuffle filenames.
+-nopalette : Do not shuffle palette AKA colors.
+
+-==TODO==-
+*Shuffle audio and movie (probably avi) files.
---
+++ RPGMaker2kXRandomizer/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Pipes;
+using System.Media;
+using System.Threading;
+using System.Reflection;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading.Tasks;
+
+namespace RPGMaker2kXRandomizer
+{ 
+    public static class Program
+    {
+        public static IEnumerable<FileInfo> GetFilesByExtensions (this DirectoryInfo dir, params string[] extensions)
+        {
+            if (extensions == null)
+                throw new ArgumentNullException("extensions");
+            IEnumerable<FileInfo> files = dir.EnumerateFiles();
+            return files.Where(f => extensions.Contains(f.Extension));
+        }
+
+        static void Shuffle<T> (this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = new Random().Next(n + 1);
+
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        static string CurrentPath = AppDomain.CurrentDomain.BaseDirectory;
+        static bool ShufflePalettes = true;
+        static bool ShuffleFilenames = false;
+
+        public static void Main (string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].ToLower() == "-nopalette") ShufflePalettes = false;
+                if (args[i].ToLower() == "-name") ShuffleFilenames = true;
+            }
+
+            if (Directory.Exists(CurrentPath + "Randomized\\"))
+            {
+                foreach (var file in new DirectoryInfo(CurrentPath + "Randomized\\").GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (var dir in new DirectoryInfo(CurrentPath + "Randomized\\").GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+            }
+            for (int i = 0; i < 19; i++ )
+            {
+                string[] folder = {"Backdrop",
+                                      "Battle",
+                                      "Battle2",
+                                      "BattleCharSet",
+                                      "BattleWeapon",
+                                      "CharSet",
+                                      "ChipSet",
+                                      "FaceSet",
+                                      "Frame",
+                                      "GameOver",
+                                      "Monster",
+                                      "Movie",
+                                      "Music",
+                                      "Panorama",
+                                      "Picture",
+                                      "Sound",
+                                      "System",
+                                      "System2",
+                                      "Title"
+                                  };
+
+                if (!Directory.Exists(CurrentPath + folder[i]))
+                {
+                    Console.WriteLine(folder[i] + " doesn't exist, skipping.");
+                    continue;
+                }
+                else
+                {
+                    Directory.CreateDirectory(CurrentPath + "Randomized\\" + folder[i]);
+                    List<string> files = new List<string>();
+                    List<Bitmap> images = new List<Bitmap>();
+                    List<ColorPalette> palettes = new List<ColorPalette>();
+
+                    foreach (var file in new DirectoryInfo(CurrentPath + folder[i]).GetFilesByExtensions(".png", ".bmp"))
+                    {
+                        files.Add(file.Name);
+                        images.Add(new Bitmap(file.FullName));
+                    }
+                    Console.WriteLine(folder[i] + " - Images added");
+
+                    foreach (var img in images)
+                    {
+                        palettes.Add(img.Palette);
+                    }
+
+                    Console.WriteLine(folder[i] + " - Palettes added");
+
+                    if (ShuffleFilenames)
+                    {
+                        files.Shuffle();
+                        Console.WriteLine(folder[i] + " - File names shuffled");
+                    }
+                    if (ShufflePalettes)
+                    {
+                        palettes.Shuffle();
+                        Console.WriteLine(folder[i] + " - Palettes shuffled");
+                    }
+
+                    for (int j = 0; j < files.Count; j++)
+                    {
+                        images[j].Palette = palettes[j];
+                        images[j].Save(CurrentPath + "Randomized\\" + folder[i] + "\\" + files[j]);
+                        //images[j].Dispose();
+                    }
+                }
+            }
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+    }
+}
